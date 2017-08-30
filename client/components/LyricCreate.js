4d8759709ea857867af3d44b89e04dccefc57d53
David Henley <henleydr@gmail.com>
2017-08-30 15:59:04 -0500

    LyricCreate

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricCreate extends Component {
+  state = {
+    content: ''
+  }
+
+  onSubmitForm = e => {
+    e.preventDefault();
+
+    const { content } = this.state;
+    const { songId } = this.props;
+
+    this.props.mutate({
+      variables: { content, songId }
+    }).then(() => this.setState({ content: '' }));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmitForm}>
+        <label>Add a Lyric</label>
+        <input
+          onChange={e => this.setState({ content: e.target.value })}
+          value={this.state.content}
+        />
+      </form>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation addLyricToSong($content: String, $songId: ID) {
+    addLyricToSong(content: $content, songId: $songId) {
+      id
+      lyrics {
+        content
+      }
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricCreate);
