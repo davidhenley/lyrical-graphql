f507dedd22a6c69def65756d549fd8fc32e294ca
David Henley <henleydr@gmail.com>
2017-08-30 21:45:17 -0500

    Setting Optimistic Response

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -3,9 +3,17 @@ import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
 class LyricList extends Component {
-  onLikeLyric(id) {
+  onLikeLyric(id, likes) {
     this.props.mutate({
-      variables: { id }
+      variables: { id },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
     });
   }
 
@@ -16,7 +24,7 @@ class LyricList extends Component {
         <div className="vote-box">
           <i
             className="material-icons"
-            onClick={() => this.onLikeLyric(id)}
+            onClick={() => this.onLikeLyric(id, likes)}
           >
             thumb_up
           </i>
